648e7c564c5da9b44a8c9f786737f8c8c39176ad
Shivanshu Chaudhary <shivanshuchaudhary97@gmail.com>
2019-03-11 23:31:46 +0530

    Solved Question 17

--- 16/solution.cpp
+++ 16/solution.cpp
@@ -1,18 +1,106 @@
 /*
 
-There are N pots. Every pot has some water in it. 
-They may be partially filled. 
+There are N pots. Every pot has some water in it.
+They may be partially filled.
 Every pot is associated with overflow number O which tell how many minimum no. of stones required for that pot to overflow.
-The crow knows O1 to On(overflow no. for all the pots). Crow wants some K pots to be overflow. 
+The crow knows O1 to On(overflow no. for all the pots). Crow wants some K pots to be overflow.
 So the task is the minimum number of stones he can make K pots overflow in the worst case.
 
 An array of overflow no--. {1,...On}
 Number of pots--n
 No of pots to overflow-- k
 
-Let say two pots are there with overflow no.s {5,58}, and the crow has to overflow one pot(k=1). 
-So crow will put 5 stones in the pot with overflow no.(58), it will not overflow, then he will put them in the pot with overflow no.(5), 
+Let say two pots are there with overflow no.s {5,58}, and the crow has to overflow one pot(k=1).
+So crow will put 5 stones in the pot with overflow no.(58), it will not overflow, then he will put them in the pot with overflow no.(5),
 hence the total no. of stones to make overflow one pot is=10.
 What is the best algorithm to do it?
 
 */
+#include <iostream>
+using namespace std;
+
+#define lim 100
+int overflow_numbers[lim];
+int k;
+int num_pots;
+
+
+
+
+void merger(int l, int m, int r){
+    int i,j,k;
+    int n1=m-l+1;
+    int n2=r-m;
+    int L[n1], R[n2];
+    for(int i=0; i<n1; i++){
+        L[i]=overflow_numbers[l+i];
+    }
+    for(int j=0; j<n2; j++){
+        R[j]=overflow_numbers[m+1+j];
+    }
+
+
+    i=0;
+    j=0;
+    k=l;
+    while(i<n1 && j<n2){
+        if(L[i]<=R[j]){
+            overflow_numbers[k]=L[i];
+            i++;
+        }else{
+            overflow_numbers[k]=R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<n1){
+        overflow_numbers[k]=L[i];
+        k++;
+        i++;
+    }
+    while(j<n2){
+        overflow_numbers[k]=R[j];
+        k++;
+        j++;
+    }
+}
+
+
+void merge_sort(int l, int r){
+    if(l<r){
+        int m=l+(r-l)/2;
+        merge_sort(l, m);
+        merge_sort(m+1, r);
+        merger(l, m, r);
+    }
+}
+
+
+
+int main(){
+    cin>>num_pots;
+    for(int i=0; i<num_pots; i++){
+        int x;
+        cin>>x;
+        overflow_numbers[i]=x;
+    }
+    cin>>k;
+    merge_sort(0,num_pots-1);
+
+
+
+    for(int i=num_pots-1; i>0; i--){
+        overflow_numbers[i] = max(0,overflow_numbers[i]-overflow_numbers[i-1]);
+    }
+
+
+
+    int total_stones=0;
+    for(int i=0; i<k; i++){
+        total_stones+=(overflow_numbers[i]*(num_pots-i));
+    }
+    cout<<"\nTotal Stones Required : "<<total_stones<<endl;
+
+}
+
+
--- 17/solution.cpp
+++ 17/solution.cpp
@@ -1,7 +1,7 @@
 /*
 
-Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums. 
-You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins. 
+Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums.
+You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins.
 Here left and right are adjacent indices of i. After the burst, the left and right then becomes adjacent.
 Find the maximum coins you can collect by bursting the balloons wisely.
 
@@ -18,3 +18,54 @@ Return 167
 nums = [3,1,5,8] –> [3,5,8] –> [3,8] –> [8] –> [] coins = 3*1*5 + 3*5*8 + 1*3*8 + 1*8*1 = 167
 
 */
+
+
+
+#include <iostream>
+using namespace std;
+
+#define lim 500
+int bal[lim];
+int dp[lim][lim];
+int n;
+
+
+int solution(){
+    for(int len=1; len<=n; len++){
+        for(int i=0; i<=n-len; i++){
+            int j=i+len-1;
+            for(int k=i; k<=j; k++){
+                int leftVal=1;
+                int rightVal=1;
+
+                if(i!=0){
+                    leftVal=bal[i-1];
+                }
+                if(j!=n-1){
+                    rightVal=bal[j+1];
+                }
+
+                int before=0;
+                int after=0;
+                if(i!=k){
+                    before=dp[i][k-1];
+                }
+                if(j!=k){
+                    after=dp[k+1][j];
+                }
+                dp[i][j]=max(dp[i][j], before+after+(leftVal*rightVal*bal[k]));
+            }
+        }
+    }
+    return dp[0][n-1];
+}
+
+int main(){
+    cin>>n;
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        bal[i]=x;
+    }
+    cout<<solution()<<endl;
+}
